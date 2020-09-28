5034999e50bf53feb3575a1cb9ac7864ce153d9c
aledeltoro <alejandroencarnacion1@gmail.com>
2020-09-28 09:04:24 -0400

    Initial commit

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -2,8 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString:
-    "postgres://dzevhhjyruavzo:bb5e81f4f13cba165490da4d5c66316481379096d02e9e0e5cac27696d3d7994@ec2-35-169-92-231.compute-1.amazonaws.com:5432/dce869kcj9h85g",
+  connectionString: process.env.DB_URI,
   ssl: {
     rejectUnauthorized: false,
   },
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const db = require("./db/postgres");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const results = await db.query("SELECT 1");
+    res.status(200).send({
+      Title: "Hello World",
+      Rows: results.rows[0],
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
