546880575a5cc78cf43883a058badbb8dff5f7f4
aledeltoro <alejandroencarnacion1@gmail.com>
2020-09-28 01:08:09 -0400

    Add initial project structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
