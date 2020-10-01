791e0c2a8c42cfb84f968b11e3e7d43414d7068c
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-01 19:35:46 -0400

    Fixed small issue with logout

--- src/controllers/users/logout.js
+++ src/controllers/users/logout.js
@@ -4,9 +4,11 @@ require("dotenv").config();
 
 const logout = async (req, res) => {
     const {token} = req.body;
-
+    if(!token){
+        return res.sendStatus(401);
+    }
     try{
-        await db.query('DELETE FROM Session WHERE RefreshToken = $1', [data.id_contact]);
+        await db.query('DELETE FROM Session WHERE RefreshToken = $1', [token]);
         res.status(200).send({message: 'Logout successful'});
     } catch(error){
         res.status(500).send({error: 'An error ocurred trying to log out'});
