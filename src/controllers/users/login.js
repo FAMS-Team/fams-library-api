3ba50c1e5a42494950af002f615cc62b5a05f772
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-01 19:16:50 -0400

    Fixed refresh tokens

--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -13,7 +13,13 @@ const login = async (req, res) => {
         if (await bcrypt.compare(password, user.password)){
             const accessToken = jwt.sign(user, process.env.ACCESS_KEY, {expiresIn: '30m'});
             const refreshToken = jwt.sign(user, process.env.REFRESH_KEY);
-            db.query('INSERT INTO Session (ID_Contact, RefreshToken) VALUES ($1, $2)', [user.id_contact, refreshToken]);
+            const session = await db.query('SELECT RefreshToken FROM Session WHERE ID_Contact = $1', [user.id_contact]);
+            if (session.rowCount > 0){
+                await db.query('UPDATE Session SET RefreshToken = $1 WHERE ID_Contact = $2', [refreshToken, user.id_contact]);
+            }
+            else{
+                await db.query('INSERT INTO Session (ID_Contact, RefreshToken) VALUES ($1, $2)', [user.id_contact, refreshToken]);
+            }
             res.status(200).send({accessToken, refreshToken});
         }
         else{
