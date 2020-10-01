89b9fb135dcacc59ca5142d80566b03601f4cfd2
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-01 15:54:51 -0400

    Solved merge problem in books.js

--- src/routes/books.js
+++ src/routes/books.js
@@ -7,13 +7,7 @@ const readBookByID = require("../controllers/books/read");
 const deleteBookByID = require("../controllers/books/delete")
 
 // Routes
-<<<<<<< HEAD
-router.post("/books", createBook);
-router.get("/books/:id", readBookByID);
-router.delete("/books/:id", deleteBookByID);
-=======
 router.post("/create", createBook);
 router.get("/:id", readBookByID);
->>>>>>> 684d551c4cdf13fcbcfcf467e6f79709aa50edc5
 
 module.exports = router;
