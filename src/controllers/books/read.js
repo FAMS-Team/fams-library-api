c122b65f6a48820babb0af61bc22f885fa4e898d
Banano-Cry <markbenitezk@gmail.com>
2020-10-02 15:31:41 -0400

    Create in book controller (read) const getBooksWithoutBookLink and getBookByIDWithoutBookLink; Add in db (queries) two new queries

--- src/controllers/books/read.js
+++ src/controllers/books/read.js
@@ -18,6 +18,23 @@ const getBookByID = async (req, res) => {
   }
 };
 
+const getBookByIDWithoutBookLink = async (req,res) => {
+  const id = req.params.id;
+
+  try{
+    const result = await db.query(queries.selectBookInnerJoinWithoutBookLink, [id]);
+
+    if(result.rows.length == 0){
+      return res.status(404).send();
+    }
+
+    const book = result.rows[0];
+    res.status(200).send(book);
+  } catch (err) {
+    res.status(500).send();
+  }
+};
+
 const getBooks = async (req, res) => {
   try {
     const books = await db.query(queries.selectAllBooks);
@@ -27,4 +44,13 @@ const getBooks = async (req, res) => {
   }
 };
 
-module.exports = { getBookByID, getBooks };
+const getBooksWithoutBookLink = async (req,res) => {
+  try {
+    const books = await db.query(queries.selectAllBooksWithoutBookLink);
+    res.status(200).send(books.rows);
+  } catch (err) {
+    res.status(500).send();
+  }
+};
+
+module.exports = { getBookByID, getBooks,getBookByIDWithoutBookLink,getBooksWithoutBookLink };
