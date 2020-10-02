5d0cef8c29909e7e365ff13c21e9aa13a0877007
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-01 22:39:53 -0400

    Get categories now shows category id

--- src/controllers/categories/read.js
+++ src/controllers/categories/read.js
@@ -2,21 +2,21 @@ const db = require('../../db/postgres');
 
 const categories = async (req, res) => {
     try{
-        const result = await db.query('SELECT C.Name AS Category, S.Name AS Subcategory FROM BookCategory AS C INNER JOIN BookSubcategory AS S ON C.ID_BookCategory = S.ID_BookCategory');
+        const result = await db.query('SELECT C.Name AS Category, S.ID_BookSubcategory, C.ID_BookCategory, S.Name AS Subcategory FROM BookCategory AS C INNER JOIN BookSubcategory AS S ON C.ID_BookCategory = S.ID_BookCategory');
         
         //Arranges result in nested json
         const cats = [];
-        let category = result.rows[0].category;
+        let category = result.rows[0];
         let subcategories = [];
         result.rows.forEach(item =>{
-            if(item.category !== category) {
-                cats.push({category: category, subcategories: subcategories});
-                category = item.category;
+            if(item.id_bookcategory !== category.id_bookcategory) {
+                cats.push({id: category.id_bookcategory, category: category.category, subcategories: subcategories});
+                category = item;
                 subcategories = [];
             }
-            subcategories.push(item.subcategory);
+            subcategories.push({id: item.id_booksubcategory, subcategory: item.subcategory});
         });
-        cats.push({category: category, subcategories: subcategories})
+        cats.push({id: category.id_bookcategory, category: category.category, subcategories: subcategories});
 
         res.status(200).json(cats);
     }
