f70867e404b28485654d3d57f7bf56f2c04f4743
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-03 19:49:13 -0400

    Added update publisher route

--- src/routes/publishers.js
+++ src/routes/publishers.js
@@ -4,10 +4,12 @@ const router = express.Router();
 // Controllers
 const getPublishers = require("../controllers/publishers/read");
 const insertPublisher = require("../controllers/publishers/create");
+const updatePublisher = require("../controllers/publishers/update");
 const authenticate = require("../controllers/users/verify");
 
 // Routes
 router.get("/publishers", getPublishers);
-router.post("/publishers/create", authenticate, insertPublisher)
+router.post("/publishers", authenticate, insertPublisher)
+router.post("/publishers/:id", authenticate, updatePublisher);
 
 module.exports = router;
