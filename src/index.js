1027c2fee5f96b8cda77728830bd5a016f58a630
Banano-Cry <markbenitezk@gmail.com>
2020-10-03 12:45:24 -0400

    Create method create (reservations, controllers), create reservation (routes) and modify index.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const bookRouter = require("./routes/books");
 const countryRouter = require("./routes/countries");
 const categoryRouter = require("./routes/categories");
 const paymentRouter = require("./routes/payments");
+const reservationRouter = require("./routes/reservations");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +20,7 @@ app.use(bookRouter);
 app.use(userRouter);
 app.use(categoryRouter);
 app.use(paymentRouter);
+app.use(reservationRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
