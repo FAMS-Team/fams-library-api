a515fb3543d93fff53e7bf0bd5f94e9322eac77d
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-03 23:26:39 -0400

    Changed get publishers to show country name

--- src/db/scripts/queries/queries_publisher.js
+++ src/db/scripts/queries/queries_publisher.js
@@ -1,8 +1,8 @@
 const selectAllPublishers = `
-    SELECT * FROM Publisher AS P
+    SELECT P.ID_Publisher, P.Name, C.Name FROM Publisher AS P
     INNER JOIN Country AS C
     ON P.ID_Country = C.ID_Country
-    ORDER BY Name
+    ORDER BY P.Name
 `
 
 const insertPublisher = `
