7b8d34e019dd98323231b04d472824805a459107
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-04 22:49:38 -0400

    Changed update method for authors controller

--- src/controllers/authors/update.js
+++ src/controllers/authors/update.js
@@ -12,9 +12,28 @@ const updateAuthor = async (req, res) => {
     else if(user.id_contacttype !== 1){
         return res.status(401);
     }
-
+    /*
     if (!name || !country || !lastname || !date_birth || !date_death || !description){
         return res.status(400).send({error: 'Insufficcient details provided.'})
+    }
+    */
+    if (name){
+        await db.query(queries.updateAuthorName, [name, author]);
+    }
+    if (lastname){
+        await db.query(queries.updateAuthorLastName, [lastname, author]);
+    }
+    if (date_birth){
+        await db.query(queries.updateAuthorDateBirth, [date_birth, author]);
+    }
+    if (date_death){
+        await db.query(queries.updateAuthorDateDeath, [date_death, author]);
+    }
+    if (description){
+        await db.query(queries.updateAuthorDescription, [description, author]);
+    }
+    if (country){
+        await db.query(queries.updateAuthorCountry, [country, author]);
     }
     try{
         await db.query(queries.updateAuthor, [name, lastname, date_birth, date_death, description, country, author]);
--- src/db/scripts/queries/queries_author.js
+++ src/db/scripts/queries/queries_author.js
@@ -9,7 +9,7 @@ const insertAuthor = `
     INSERT INTO Author (Name, Last_Name, Date_Birth, Date_Death, Description, ID_Country)
     VALUES ($1, $2, $3, $4, $5, $6)
 `
-
+/*
 const updateAuthor = `
     UPDATE Author SET 
         Name = $1,
@@ -20,9 +20,46 @@ const updateAuthor = `
         ID_Country = $6
     WHERE ID_Author = $7
 `
+*/
+
+const updateAuthorName = `
+    UPDATE Author SET Name = $1
+    WHERE ID_Author = $2
+`
+
+const updateAuthorLastName = `
+    UPDATE Author SET Last_Name = $1
+    WHERE ID_Author = $2
+`
+
+const updateAuthorDateBirth = `
+    UPDATE Author SET Date_Birth = $1
+    WHERE ID_Author = $2
+`
+
+const updateAuthorDateDeath = `
+    UPDATE Author SET Date_Death = $1
+    WHERE ID_Author = $2
+`
+
+const updateAuthorDescription = `
+    UPDATE Author SET Description = $1
+    WHERE ID_Author = $2
+`
+
+const updateAuthorCountry = `
+    UPDATE Author SET ID_Country = $1
+    WHERE ID_Author = $2
+`
 
 module.exports = {
     selectAllAuthors,
     insertAuthor,
-    updateAuthor
+    //updateAuthor
+    updateAuthorName,
+    updateAuthorLastName,
+    updateAuthorDateBirth,
+    updateAuthorDateDeath,
+    updateAuthorDescription,
+    updateAuthorCountry
 }
