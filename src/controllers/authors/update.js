c7c05c06cb36c555e2d35b94f0e73c1f2adb0779
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-04 22:53:10 -0400

    Fixed update method for publishers controller

--- src/controllers/authors/update.js
+++ src/controllers/authors/update.js
@@ -17,26 +17,26 @@ const updateAuthor = async (req, res) => {
         return res.status(400).send({error: 'Insufficcient details provided.'})
     }
     */
-    if (name){
-        await db.query(queries.updateAuthorName, [name, author]);
-    }
-    if (lastname){
-        await db.query(queries.updateAuthorLastName, [lastname, author]);
-    }
-    if (date_birth){
-        await db.query(queries.updateAuthorDateBirth, [date_birth, author]);
-    }
-    if (date_death){
-        await db.query(queries.updateAuthorDateDeath, [date_death, author]);
-    }
-    if (description){
-        await db.query(queries.updateAuthorDescription, [description, author]);
-    }
-    if (country){
-        await db.query(queries.updateAuthorCountry, [country, author]);
-    }
     try{
-        await db.query(queries.updateAuthor, [name, lastname, date_birth, date_death, description, country, author]);
+        if (name){
+            await db.query(queries.updateAuthorName, [name, author]);
+        }
+        if (lastname){
+            await db.query(queries.updateAuthorLastName, [lastname, author]);
+        }
+        if (date_birth){
+            await db.query(queries.updateAuthorDateBirth, [date_birth, author]);
+        }
+        if (date_death){
+            await db.query(queries.updateAuthorDateDeath, [date_death, author]);
+        }
+        if (description){
+            await db.query(queries.updateAuthorDescription, [description, author]);
+        }
+        if (country){
+            await db.query(queries.updateAuthorCountry, [country, author]);
+        }
+        //await db.query(queries.updateAuthor, [name, lastname, date_birth, date_death, description, country, author]);
         return res.status(200).send({message: 'Author updated successfuly.'});
     } catch (error){
         return res.status(500).send(error);
