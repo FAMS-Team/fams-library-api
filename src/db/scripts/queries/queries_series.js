d71f9bf3ea0128d7fd50d2e314df6e75f0954ff7
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-05 11:19:34 -0400

    Added create series controller and route

--- src/db/scripts/queries/queries_series.js
+++ src/db/scripts/queries/queries_series.js
@@ -2,6 +2,12 @@ const selectAllSeries = `
 	SELECT * FROM Series ORDER BY Name
 `;
 
+const insertSeries = `
+    INSERT INTO Series (Name)
+    VALUES ($1)
+`
+
 module.exports = {
-    selectAllSeries
+    selectAllSeries,
+    insertSeries
 }
