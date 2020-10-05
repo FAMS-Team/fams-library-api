c133f789a51d5b23fcbf694bd621907d4d9dbcb4
Banano-Cry <markbenitezk@gmail.com>
2020-10-05 15:21:14 -0400

    Add method getBook without BookLink in read bookeditions

--- src/controllers/bookeditions/read.js
+++ src/controllers/bookeditions/read.js
@@ -17,4 +17,20 @@ const getBookEditionByID = async (req, res) => {
   }
 };
 
+const getBookEditionByIDWithoutBookLink = async (req, res) => {
+  const id = req.params.id;
+
+  try{
+    const result = await db.query(queries.selectBookEditionWithoutBookLink,[id]);
+
+    if(result.rows.length == 0){
+      return res.status(404).send();
+    }
+    const book = result.rows[0];
+    res.status(200).send(book);
+  }catch(err) {
+    res.status(500).send();
+  }
+};
+
 module.exports = getBookEditionByID
--- src/db/scripts/queries/queries_bookedition.js
+++ src/db/scripts/queries/queries_bookedition.js
@@ -80,6 +80,32 @@ const selectBookEdition = `
 		WHERE BE.id_publisher_book = $1
 `;
 
+const selectBookEditionWithoutBookLink = `
+	SELECT
+		B.title, B.subtitle, B.description, B.publication_date, BSC.name, BC.name, S.name,
+		A.name, A.last_name, P.name, BE.edition, BE.page_number, BE.isbn, BE.price,
+		BE.image_link
+	FROM
+		book AS B
+		INNER JOIN booksubcategory AS BSC
+			ON BSC.id_booksubcategory = B.id_booksubcategory
+		INNER JOIN bookcategory AS BC
+			ON BC.id_bookcategory = BSC.id_booksubcategory
+		INNER JOIN series AS S
+			ON B.id_series = S.id_series
+		INNER JOIN book_author AS BA
+			ON BA.id_book = B.id_book
+		INNER JOIN author AS A
+			ON A.id_author = BA.id_author
+		INNER JOIN publisher_book AS PB
+			ON PB.id_book = B.id_book
+		INNER JOIN publisher AS P
+			ON P.id_publisher = PB.id_publisher
+		INNER JOIN bookedition AS BE
+			ON BE.id_publisher_book = PB.id_publisher_book
+		WHERE BE.id_publisher_book = $1
+`;
+
 module.exports = {
   insertBookEdition,
   insertBookPublisher,
@@ -89,5 +115,6 @@ module.exports = {
 	selectBookPublisherByID,
 	selectBookPublisherInBookEdition,
 	deleteBookPublisherByID,
-	selectBookEdition
+	selectBookEdition,
+	selectBookEditionWithoutBookLink
 };
