277cf3fffa46fc13099ee3fb634e25caacfb2efc
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-07 19:52:59 -0400

    Added manual config to cloudinary

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require('cors');
+require("dotenv").config();
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+  cloud_name: 'fams-library', 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET 
+});
 
 // Routers
 const userRouter = require("./routes/user");
