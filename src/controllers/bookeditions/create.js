941b55a650282b668e7a3ca75660fdf7f75edc63
aledeltoro <alejandroencarnacion1@gmail.com>
2020-10-13 22:20:53 -0400

    Add conditionals in case image and books are undefined

--- src/controllers/bookeditions/create.js
+++ src/controllers/bookeditions/create.js
@@ -1,35 +1,39 @@
 const db = require("../../db/postgres");
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 const queries = require("../../db/scripts/queries/queries_bookedition");
 
-const createBookEdition = async (req,res) => {
+const createBookEdition = async (req, res) => {
   const bookEdition = new BookEdition(req.body);
   bookEdition.bookID = req.params.id_book;
   const type = req.user.id_contacttype;
-  if(type !== 1){
+  if (type !== 1) {
     res.sendStatus(403);
-  }
-  else{
-    try{
-      await db.query('BEGIN');
+  } else {
+    try {
+      await db.query("BEGIN");
       let result = await db.query(queries.selectIDPublisherBook, [
         bookEdition.bookID,
-        bookEdition.publisherID
+        bookEdition.publisherID,
       ]);
       //let publisherBookID = result.rows[0].id_publisher_book
-      
-      if (result.rowCount === 0){
+
+      if (result.rowCount === 0) {
         result = await db.query(queries.insertBookPublisher, [
           bookEdition.bookID,
-          bookEdition.publisherID
+          bookEdition.publisherID,
         ]);
         publisherBookID = result.rows[0].id_publisher_book;
       }
-      
-      const image = await cloudinary.uploader.upload(bookEdition.imageLink);
-      const book = await cloudinary.uploader.upload(bookEdition.bookLink);
-      bookEdition.imageLink = image.secure_url;
-      bookEdition.bookLink = book.secure_url;
+
+      if (image.length != 0) {
+        const image = await cloudinary.uploader.upload(bookEdition.imageLink);
+        bookEdition.imageLink = image.secure_url;
+      }
+
+      if (book.length != 0) {
+        const book = await cloudinary.uploader.upload(bookEdition.bookLink);
+        bookEdition.bookLink = book.secure_url;
+      }
 
       await db.query(queries.insertBookEdition, [
         bookEdition.edition,
@@ -40,10 +44,10 @@ const createBookEdition = async (req,res) => {
         bookEdition.bookLink,
         publisherBookID,
       ]);
-      await db.query('COMMIT');
-      res.status(201).send({message: "Successfully added edition!"});
+      await db.query("COMMIT");
+      res.status(201).send({ message: "Successfully added edition!" });
     } catch (err) {
-      await db.query('ROLLBACK');
+      await db.query("ROLLBACK");
       res.status(400).send(err);
     }
   }
@@ -58,6 +62,6 @@ function BookEdition(body) {
   this.bookLink = body.book_link;
   this.publisherID = body.publisher_id;
   this.bookID = body.book_id;
-};
+}
 
 module.exports = createBookEdition;
